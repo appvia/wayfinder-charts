fecae4504dc6351444974e8ba1e1875f345bebca
Mark Hughes <mrsheepuk@users.noreply.github.com>
2024-10-24 14:15:28 +0100

    Fix dockerfile and charts URL override script (#9)

--- charts/set-charts-url.sh
+++ charts/set-charts-url.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-CHARTS_URL="${CHARTS_PROTOCOL:-http}:\/\/${CHARTS_HOSTNAME}"
+CHARTS_URL="${CHARTS_PROTOCOL:-http}://${CHARTS_HOSTNAME}"
 
-sed s/https:\/\/charts\.wayfinder\.run/$CHARTS_URL/g /usr/share/nginx/html/index.yaml.tmpl > /usr/share/nginx/html/index.yaml
+sed s~https://charts.wayfinder.run~$CHARTS_URL~g /usr/share/nginx/html/index.yaml.tmpl > /usr/share/nginx/html/index.yaml
